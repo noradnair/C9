7e25b21876e73790c3a114fb8dd33083ce118396
noradnair <93444534+noradnair@users.noreply.github.com>
2021-10-30 11:51:31 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+
+function setup() {
+  createCanvas(400,400);
+  box=createSprite(200,200,30,30)
+}
+
+function draw() 
+{
+  background(30);
+  if (keyDown(DOWN_ARROW)) {
+    box.y=box.y+5
+  }
+  if (keyDown(UP_ARROW)) {
+    box.y=box.y-5
+  }
+drawSprites()
+}
+  
+
+
+
